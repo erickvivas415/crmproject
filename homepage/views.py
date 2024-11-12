d77ab3dfdaa2d10b3aae7e9b314737f3a132a2e4
Erick Vivas <erickvivas@ericks-laptop.gateway.sonic.net>
2024-11-11 18:02:11 -0800

    Second commit

--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'homepage/index.html', {})
+
+def home(request):
+    return render(request, 'homepage/home.html', {})
+
+def profile(request):
+    return render(request, 'homepage/profile.html', {})
