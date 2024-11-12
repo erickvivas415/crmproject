54c768e962400d551df84da4f79211cb3f382d1d
Erick Vivas <erickvivas@ericks-laptop.gateway.sonic.net>
2024-11-11 18:41:58 -0800

    Media path correction in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,6 +146,5 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / 'media'
-
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/' 
