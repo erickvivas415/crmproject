d7adae4328777acbcf240541f37408b815defc09
Erick Vivas <erickvivas@ericks-laptop.gateway.sonic.net>
2025-02-05 09:26:33 -0800

    Fix media path

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        #default='postgresql://postgres_1e0c_user:ynC5dlpoqA4fG4hMHGGkVisIUA1qVVmx@dpg-cuhf5glumphs73fj0r10-a.oregon-postgres.render.com/postgres_1e0c',
-        default='postgresql://postgres:Maier1979@localhost:5432/postgres',        
-        conn_max_age=600 
+        default='postgresql://postgres_1e0c_user:ynC5dlpoqA4fG4hMHGGkVisIUA1qVVmx@dpg-cuhf5glumphs73fj0r10-a.oregon-postgres.render.com/postgres_1e0c',
+        #default='postgresql://postgres:Maier1979@localhost:5432/postgres',        
+        #conn_max_age=600 
         )
     }
 
@@ -148,5 +148,5 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/' 
