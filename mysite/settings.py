ca69d22ba2d2b3f00efc780d9ddcd017c95cd7c4
Erick Vivas <erickvivas@mac.gateway.sonic.net>
2025-02-17 18:31:46 -0800

    Trying static fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres_1e0c_user:ynC5dlpoqA4fG4hMHGGkVisIUA1qVVmx@dpg-cuhf5glumphs73fj0r10-a.oregon-postgres.render.com/postgres_1e0c',
-        #default='postgresql://postgres:Maier1979@localhost:5432/postgres',        
-        #conn_max_age=600 
+        #default='postgresql://postgres_1e0c_user:ynC5dlpoqA4fG4hMHGGkVisIUA1qVVmx@dpg-cuhf5glumphs73fj0r10-a.oregon-postgres.render.com/postgres_1e0c',
+        default='postgresql://postgres:Maier1979@localhost:5432/postgres',        
+        conn_max_age=600 
         )
     }
 
@@ -134,14 +134,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
