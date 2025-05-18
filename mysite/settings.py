56baf69a7973dd50bfe1b4ea30c5a8ec673a873f
Erick Vivas <erickvivas@mac.gateway.sonic.net>
2025-05-17 17:21:41 -0700

    Update colors + AI chatbot

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Load environment variables from the .env file
 #load_dotenv()
